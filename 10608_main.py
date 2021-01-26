3f8cdcf11dc5baabb062ef6848b8e6ee644f0c1b
f.luetkebohmert <f.luetkebohmert@huettinger.de>
2021-01-26 13:30:46 +0100

    - deleted unused stuff

--- 10608_main.py
+++ 10608_main.py
@@ -1,22 +1,26 @@
 import RPi.GPIO as GPIO
 import time
 import pygame
-import sys
-import glob
-import os
-import random
-from rpi_backlight import Backlight
-from pygame.mixer  import *
+
 ##################################
 
 DEBUG               = False
-
-COLOR_BG            = '#59b9f5'
+COLOR_BG            = '#ffffff'
 FPS                 = 25
-BUTTONS_COUNT       = 6
-MAX_FREQUENCE_STEPS = 10
+
+##################################
+RUNNING        = False
+global start_time
+global last_grayCode
+##################################
+
+PINS_IN         = [4, 17, 27, 22]
+PINS_OUT        = [ 5, 6, 13, 19]
+BUTTON_NAMES    = ['Nuclear', 'Battery', 'Solar', 'Infeasible']
+GRAYCODE_PINS   = [25, 18, 23, 24]
 
 PLANET_NAME = [
+    'Sun'
     'Mercury',
     'Venus',
     'Earth',
@@ -41,6 +45,7 @@ TEXTS = {
     'Kuiper Belt': ["","",""]
 }
 
+
 GRAYCODE_TABLE = [
     '0000',
     '0001',
@@ -59,37 +64,6 @@ GRAYCODE_TABLE = [
     '1000'
 ]
 
-PINS_IN = {
-    'Nuclear': 4,
-    'Battery': 17,
-    'Solar': 27,
-    'Nothing': 22,
-    'GreyCode_0': 26,
-    'GreyCode_1': 18,
-    'GreyCode_2': 23,
-    'GreyCode_3': 24,
-    }
-
-PINS_OUT = {
-    'Nuclear': 5,
-    'Battery': 6,
-    'Solar': 13,
-    'Infeasible': 19,
-    }
-
-##################################
-RUNNING        = False
-audiofiles     = []
-btnAudioIndies = []
-dir            = "/home/pi/Desktop/huettinger"
-
-isOn          = False
-pressedPower  = False
-pressedPrev   = False
-pressedNext   = False
-audioIndex    = 0
-##################################
-
 def drawText(_screen, _text, _x, _y, _font, _color, _isRight):
     try:
         font = _font
@@ -106,192 +80,99 @@ def drawText(_screen, _text, _x, _y, _font, _color, _isRight):
         print('Font Error, saw it coming')
         raise e
 
-def changeAudio(_index):
-    if (pygame.mixer.music.get_busy() == True):
-        #pygame.mixer.fadeout(250)
-        pygame.mixer.music.stop()
-        print('stop')
-    
-    if _index > -1 and _index < len(audiofiles):
-        pygame.mixer.music.load(audiofiles[_index])
-        pygame.mixer.music.play(-1)
-        print('playing')
-
 def changeLight(_index):
     chIndex = 0
-    while chIndex < BUTTONS_COUNT:
+    while chIndex < len(PINS_OUT):
+        GPIO.output(PINS_OUT[chIndex], 0)
         chIndex = chIndex + 1
-        GPIO.output(PINS_OUT['CH_' + str(chIndex)], 0)
         
-    if _index < BUTTONS_COUNT and _index > -1:
-        GPIO.output(PINS_OUT['CH_' + str( _index + 1)], 1)      
+    if _index < len(PINS_OUT) and _index > -1:
+        GPIO.output(PINS_OUT[_index], 1)          
 
-def setPower(_on):
-    Backlight().power = _on
-    if _on:
-        GPIO.output(PINS_OUT['POWER'], 0)
-        audioIndex = 0       
-        changeLight(btnAudioIndies[audioIndex])
-        changeAudio(audioIndex)
-    else:
-        GPIO.output(PINS_OUT['POWER'], 1)
-        changeLight(-1)
-        changeAudio(-1)
+def turnOnAllLights():
+    for pin in PINS_OUT:
+        GPIO.output(pin, 1)
 
 def update():
     global start_time
-    global isOn
-    global pressedPower
-    global pressedPrev
-    global pressedNext
-    global audioIndex
+    global last_grayCode
+    
     
     # fills the screen with a color 
     screen.fill(pygame.Color(COLOR_BG))   
     updated_text = time.strftime("Press a button - %M:%S.", time.gmtime())
-    screen.blit(img_net, (700,20))
+    #screen.blit(img_net, (700,20))
     
-    #POWER
-    if GPIO.input(PINS_IN['POWER']) and not pressedPower:
+    graycode = []
+    for i in range(len(GRAYCODE_PINS)):
+        if GPIO.input[GRAYCODE_PINS[i]]:
+            graycode[i] = 1
+        else:
+            graycode[i] = 0
+    print("GRAYCODE :")
+    print(*graycode)
+    if last_grayCode != graycode:
+        last_grayCode = graycode
         start_time = time.time()
-        pressedPower = True
-        isOn = not isOn
-        setPower(isOn)
-        print('Power is ' + str(isOn) )
-        if not isOn:
-            changeAudio(-1)
-    elif not GPIO.input(PINS_IN['POWER']) and pressedPower:
-        pressedPower = False
-    
-    if not isOn:
-        return True
-    
-    #NEXT
-    if GPIO.input(PINS_IN['NEXT']) and not pressedPrev:
-        pressedPrev = True
-        GPIO.output(PINS_OUT['NEXT'], 1)
-        audioIndex = audioIndex - 1
-                    
-        if audioIndex < 0:
-            audioIndex = 0
-            
-        changeLight(btnAudioIndies[audioIndex])
-        changeAudio(audioIndex)
-        print('audioIndex is ' + str(audioIndex) )
-        
-    elif not GPIO.input(PINS_IN['NEXT']) and pressedPrev:
-        pressedPrev = False
-        GPIO.output(PINS_OUT['NEXT'], 0)
-    
-    #PREV
-    if GPIO.input(PINS_IN['PREV']) and not pressedNext:
-        pressedNext = True
-        GPIO.output(PINS_OUT['PREV'], 1)
-        audioIndex = audioIndex + 1
-        
-        if audioIndex > len(audiofiles) - 1:
-            audioIndex = len(audiofiles) - 1
-        
-        changeLight(btnAudioIndies[audioIndex])
-        changeAudio(audioIndex)
-        print('audioIndex is ' + str(audioIndex) )
-        
-    elif not GPIO.input(PINS_IN['PREV']) and pressedNext:
-        pressedNext = False
-        GPIO.output(PINS_OUT['PREV'], 0)
+    else:
+        print("same graycode")
 
-    for key in PINS_IN:
-        if GPIO.input(PINS_IN[key]):        
-            if key.find('CH_') > -1:                
-                findIndex = 0
-                btnIndex = int(key.replace('CH_', '')) - 1;
-                print("btnIndex " + str(btnIndex) )
-                while findIndex < len(btnAudioIndies):
-                    if btnAudioIndies[findIndex] == btnIndex:
-                        audioIndex = findIndex
-                        break
-                    findIndex = findIndex + 1
-                    
-                changeLight(btnIndex)
-                changeAudio(audioIndex)
-                
-            if DEBUG:
-                drawText(screen, 'PIN ' + key, 400, 20, font_1, color_white, False)
-        #else:
-            #GPIO.output(PINS_OUT[key], 0)
+    text_arr= []
+    for ind in range(len(GRAYCODE_TABLE)):
+        if (GRAYCODE_TABLE[ind] == str(graycode)):
+            print(ind)
+            print(PLANET_NAME[ind]) 
+            text_arr =  TEXTS[PLANET_NAME[ind]]
+
+    turnOnAllLights()
+
+    text = TEXTS['SCREENSAVER']
+    #Buttons
+    for i in range(len(PINS_IN)):
+        if GPIO.input(PINS_IN[i]):
+            print(BUTTON_NAMES[i])
+            print(text_arr[i])
+            text = text_arr[i]
+            changeLight(i)
+
+
+    drawText(screen, text, 400, 20, font, color_black, False)
+
     
-    drawText(screen, str(audioIndex), 20, 50, font_1, color_white, False)
-    drawText(screen, PLANET_TIME[audioIndex], 680, 50, font_1, color_white, True)
-    drawText(screen, SONG_NAME[audioIndex], 20, 140, font_3, color_white, False)
-    drawText(screen, INTERPRET_NAME[audioIndex], 20, 280, font_2, color_white, False)
-    
-    # updates the frames of the game 
     pygame.display.update()
 
 def main():
-    global root
     global screen
-    global color_white
-    global font_1
-    global font_2
-    global font_3
-    global img_black
+    global color_black
+    global font
     global img_net
-    global audiofiles
-    global btnAudioIndies
-    
-    time.sleep(1)
-    
-    audioCount = 0
-    files = glob.glob(dir + '/sound/*.wav')
-    if len(files) == 0:
-        files = glob.glob(dir + '/sound/*.mp3')
-  
-    for file in files:
-        audiofiles.append(file)
-        btnAudioIndies.append(audioCount)
-        audioCount = audioCount + 1
-        print(file)
-        
-    random.shuffle(audiofiles)
-    random.shuffle(btnAudioIndies)
 
-    for fileindex in btnAudioIndies:
-        print(fileindex)
+    time.sleep(1)
         
-    for file in audiofiles:
-        print(file);
-        
-    os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (0,0)
     pygame.init()
     pygame.mouse.set_cursor((8,8),(0,0),(0,0,0,0,0,0,0,0),(0,0,0,0,0,0,0,0))
-    fonts = pygame.font.get_fonts()
 
-    #for f in fonts:
-     #   print(f)
-    
     screen = pygame.display.set_mode((800, 480), pygame.NOFRAME)
-    sysfont = pygame.font.get_default_font()
     #print('system font :', sysfont)
    
     GPIO.setmode(GPIO.BCM)
     
-    for key in PINS_IN:
-        GPIO.setup(PINS_IN[key], GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
-    
-    for key in PINS_OUT:
-        GPIO.setup(PINS_OUT[key], GPIO.OUT)
-        GPIO.output(PINS_OUT[key], 0)
 
-    color_white = pygame.Color("#ffffff")
+    for pin in PINS_IN:
+        GPIO.setup(pin, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+    
+    for pin in GRAYCODE_PINS:
+        GPIO.setup(pin, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+
+    for pin in PINS_OUT:
+        GPIO.setup(pin, GPIO.OUT)
+        GPIO.output(pin, 0)
+
+    color_black = pygame.Color("#000000")
     img_net = pygame.image.load(dir + '/images/network_strength.png')   
     
-    font_1 = pygame.font.Font(pygame.font.match_font('Arial', bold=True), 30)
-    font_2 = pygame.font.SysFont('Arial', 70)
-    font_3 = pygame.font.SysFont(pygame.font.match_font('Arial'), 170)
+    font = pygame.font.Font(pygame.font.match_font('FS ALdrin'), 16)
     
-    isOn = False
-    setPower(isOn)
   
 #####################################################################
 if __name__ == '__main__':
@@ -312,10 +193,12 @@ def timeout():
 
 start_time = time.time()
 timeout_seconds = 60
+last_grayCode = ""
 
 # main game loop
 while RUNNING:
     
+    update()
 
     time.sleep(0.1)
     now_time = time.time()
@@ -324,12 +207,10 @@ while RUNNING:
     if (elapsed_time >= timeout_seconds):
         timeout()
 
-    update()
 
     for event in pygame.event.get():
         if (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
             RUNNING = False
-            setPower(True)
             pygame.quit()
             GPIO.cleanup()
             
