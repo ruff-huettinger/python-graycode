61834b37e19e6e2a905a5b47cd772b1791b556d0
f.luetkebohmert <f.luetkebohmert@huettinger.de>
2021-02-01 10:51:14 +0100

    - added textboxes to power to explore
    - added autostartscript

--- 10608_main.py
+++ 10608_main.py
@@ -1,26 +1,15 @@
 import RPi.GPIO as GPIO
 import time
 import pygame
+from itertools import chain
 
-##################################
+DEBUG = True
 
-DEBUG               = False
-COLOR_BG            = '#ffffff'
-FPS                 = 25
-
-##################################
-RUNNING        = False
-global start_time
-global last_grayCode
-##################################
-
-PINS_IN         = [4, 17, 27, 22]
-PINS_OUT        = [ 5, 6, 13, 19]
-BUTTON_NAMES    = ['Nuclear', 'Battery', 'Solar', 'Infeasible']
-GRAYCODE_PINS   = [25, 18, 23, 24]
+COLOR_BG = '#ffffff'
+FPS = 25
 
 PLANET_NAME = [
-    'Sun'
+    'Sun',
     'Mercury',
     'Venus',
     'Earth',
@@ -29,12 +18,14 @@ PLANET_NAME = [
     'Saturn',
     'Uranus',
     'Neptune',
-    'Kuiper Belt'
-    ]
+    'Kuiper Belt',
+    'Space'
+]
 
 TEXTS = {
     'SCREENSAVER' : "Select a planet and choose your energy to travel.",
-    'Mercury': ["No need to use nuclear energy at Mercury, where sunlight is very abundant.","Only small batteries needed for when spacecraft is in the shadow of Mercury.","Solar panels provided all the power needed at Mercury.  In fact, the sun is so powerful that the spacecraft needs to be shielded from sunlight (see Mariner 10 suspended spacecraft and the MESSENGER model)."],
+    'Sun': ["Select a planet and choose your energy to travel.","Select a planet and choose your energy to travel.","Select a planet and choose your energy to travel."],
+    'Mercury': ["No need to use nuclear energy at Mercury, where sunlight is very abundant.","Only small batteries needed for when spacecraft is in the shadow of Mercury.","Solar panels provided all the power needed at Mercury. In fact, the sun is so powerful that the spacecraft needs to be shielded from sunlight (see Mariner 10 suspended spacecraft and the MESSENGER model)."],
     'Venus': ["Nuclear energy would be needed for a lander lasting more than a few hours, but orbiters can use abundant sunlight for power.","Only small batteries needed for when spacecraft is in the shadow of Venus.", "Solar panels can provided power to Magellan in orbit around Venus, where sunlight is abundant.  Soviet landers used battery power to operate for a couple of hours before the intense heat destroyed the electronics. "],
     'Earth': ["Nuclear energy was used for some military satellites orbiting Earth, but its hazards upon falling to Earth make this a bad option.", "Only small batteries needed for when spacecraft is in the shadow of the Earth.", "Solar panels can provide needed power in orbit at Earth, where sunlight is plentiful."],
     'Mars': ["Nuclear energy was used on the Curiosity rover, allowing it to operate some instruments during the night.  See Curiosity model in Rocky Planets section.", "Good batteries were used on the MER rovers, to keep the spacecraft warm through the cold nights.  See MER model in Rocky Planets section.", "Solar panels provide power to numerous spacecraft while in orbit around Mars, where sunlight is still strong.  The MER rovers used solar panels to operate during the day and to charge batteries for the night.  However, dust build-up on solar panels is a big problem for long-term surface operations on Mars."],
@@ -42,177 +33,207 @@ TEXTS = {
     'Saturn': ["Nuclear energy was critical to power the Voyager missions to Saturn, as well as the Cassini mission that orbited Saturn for many years.","Batteries are needed for when spacecraft is in the shadow of Saturn or its large moons.","Solar panels could not provide enough electricity to power spacecraft at Saturn or beyond.  Sunlight is not strong enough far from the sun to provide much power."],
     'Uranus': ["Nuclear energy was critical to power the Voyager 2 mission when it flew by Uranus in 1986.","Batteries cannot store enough energy to operate much equipment at great distances from the sun.","Solar panels could not provide enough electricity to power spacecraft at Uranus or beyond.  Sunlight is not strong enough far from the sun to provide much power."],
     'Neptune':["Nuclear energy was critical to power the Voyager 2 mission when it flew by Neptune in 1989.","Batteries cannot store enough energy to operate much equipment at great distances from the sun.","Solar panels could not provide enough electricity to power spacecraft at Neptune or beyond.  Sunlight is not strong enough far from the sun to provide much power."],
-    'Kuiper Belt': ["","",""]
+    'Kuiper Belt': ["Select a planet and choose your energy to travel.","Select a planet and choose your energy to travel.","Select a planet and choose your energy to travel."],
+    'Space': ["Select a planet and choose your energy to travel.","Select a planet and choose your energy to travel.","Select a planet and choose your energy to travel."]
 }
-
 
 GRAYCODE_TABLE = [
     '0000',
+    '1000',
+    '1001',
     '0001',
     '0011',
+    '1011',
+    '1010',
     '0010',
     '0110',
-    '0111',
-    '0101',
-    '0100',
-    '1100',
-    '1101',
+    '1110',
     '1111',
-    '1010',
-    '1011',
-    '1001',
-    '1000'
 ]
 
+BUTTON_NAMES    = ['NUCLEAR', 'BATTERY', 'SOLAR', 'INFEASIBLE']
+BUTTON_PINS_IN  = [ 4, 17, 27, 22]
+BUTTON_PINS_OUT = [ 5,  6, 13, 19]
+GRAYCODE_PINS = [25, 18, 23, 24]
+
+##################################
+RUNNING        = False
+global start_time
+global last_grayCode
+
+last_grayCode = '0000'
+##################################
+
+def drawTextBox(surface, text, color, rect, font, aa=False, bkg=None):
+	rect = pygame.Rect(rect)
+	y = rect.top
+	lineSpacing = 0
+	
+	#get height of font
+	fontHeight = font.size("Tg")[1]
+	
+	while text:
+		i=1
+		
+		#determine if text will be outside area
+		if y + fontHeight > rect.bottom:
+			break
+		#determine max width of line
+		while font.size(text[:i])[0] < rect.width and i < len(text):
+			i += 1
+			
+		#if we wrapped text, then adjust the wrap to last word
+		if i < len(text):
+			i = text.rfind(" ", 0, i) + 1
+		#render line and blit to surface
+		if bkg:
+			image = font.render(text[:i], 1, color, bkg)
+			image.set_colorkey(bkg)
+		else:
+			image = font.render(text[:i], aa, color)
+			
+		surface.blit(image, (rect.left, y))
+		y += fontHeight + lineSpacing
+		
+		#remove text we blittered
+		text = text[i:]
+	return text
+
 def drawText(_screen, _text, _x, _y, _font, _color, _isRight):
-    try:
-        font = _font
-        text = font.render(str(_text), True , _color)
-        text_rect = text.get_rect()
-        text_rect.top = _y
-        if _isRight:
-            text_rect.left = _x - text.get_width()
-        else:
-            text_rect.left = _x
-            
-        _screen.blit(text, text_rect)
-    except Exception as e:
-        print('Font Error, saw it coming')
-        raise e
+	try:
+		font = _font
+		#print(wrapline(_text,font,120))
+		text = font.render(str(_text), True, _color)
+		text_rect = text.get_rect()
+		text_rect.top = _y
+		if _isRight:
+			text_rect.left = _x -text.get_width()
+		else:
+			text_rect.left = _x
+			
+		_screen.blit(text, text_rect)
+	except Exception as e:
+		print('Font Error')
+		raise e
 
 def changeLight(_index):
     chIndex = 0
-    while chIndex < len(PINS_OUT):
-        GPIO.output(PINS_OUT[chIndex], 0)
+    while chIndex < len(BUTTON_PINS_OUT):
+        GPIO.output(BUTTON_PINS_OUT[chIndex], 0)
         chIndex = chIndex + 1
         
-    if _index < len(PINS_OUT) and _index > -1:
-        GPIO.output(PINS_OUT[_index], 1)          
+    if _index < len(BUTTON_PINS_OUT) and _index > -1:
+        GPIO.output(BUTTON_PINS_OUT[_index], 1)          
 
 def turnOnAllLights():
-    for pin in PINS_OUT:
+    for pin in BUTTON_PINS_OUT:
         GPIO.output(pin, 1)
+	
+
+def init():
+	global screen
+	global font
+	
+	pygame.init()
+	screen = pygame.display.set_mode((800,480), pygame.NOFRAME)
+	
+	font = pygame.font.Font(pygame.font.match_font('FS Aldrin'), 16)
+	
+	GPIO.setmode(GPIO.BCM)
+	
+	for pin in BUTTON_PINS_IN:
+		GPIO.setup(pin, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+	
+	for pin in GRAYCODE_PINS:
+		GPIO.setup(pin, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+	
+	for pin in BUTTON_PINS_OUT:
+		print(pin)
+		GPIO.setup(pin, GPIO.OUT)
+		GPIO.output(pin, 0)
+
+def makeGrayCodeString(arr):
+	str1 = ""
+	for ele in arr:
+		str1 += str(ele)
+	return str1
+
+def findChosenPlanet(_str):
+	str_planet = 'Space'
+	for ind in range(len(GRAYCODE_TABLE)):
+		if GRAYCODE_TABLE[ind] == _str:
+			#print(ind)
+			str_planet = str(PLANET_NAME[ind])
+	return str_planet
+
+def findCorrectText(pl, _txts):
+	_txt = TEXTS['SCREENSAVER']
+	for i in range(len(BUTTON_PINS_IN)):
+		if GPIO.input(BUTTON_PINS_IN[i]):
+			_txt = _txts[i]
+			changeLight(3)
+	return _txt
 
 def update():
-    global start_time
-    global last_grayCode
+	global start_time
+	global last_grayCode
     
-    
-    # fills the screen with a color 
-    screen.fill(pygame.Color(COLOR_BG))   
-    updated_text = time.strftime("Press a button - %M:%S.", time.gmtime())
-    #screen.blit(img_net, (700,20))
-    
-    graycode = []
-    for i in range(len(GRAYCODE_PINS)):
-        if GPIO.input[GRAYCODE_PINS[i]]:
-            graycode[i] = 1
-        else:
+	screen.fill(pygame.Color(COLOR_BG))
+	
+	graycode = [0,0,0,0]
+	for i in range(len(BUTTON_NAMES)):
+		#drawText(screen, BUTTON_NAMES[i], 200, 20+i*10, font, pygame.Color("#ffffff"), False)
+		#drawText(screen, "GREYCODE " +str(i), 200, 300+i*10, font, pygame.Color("#ffffff"), False)
+		
+		if GPIO.input(BUTTON_PINS_IN[i]):
+			print(BUTTON_NAMES[i])
+			turnOnAllLights()
+			drawText(screen, BUTTON_NAMES[i] + str(BUTTON_PINS_IN[i]), 400, 20+i*10, font, pygame.Color("#ffffff"), False)
+
+		if GPIO.input(GRAYCODE_PINS[i]):
+			#drawText(screen, "GREYCODE " + str(GRAYCODE_PINS[i]), 400, 300+i*10, font, pygame.Color("#ffffff"), False)
+			graycode[i] = 1
+			
+        if not GPIO.input(GRAYCODE_PINS[i]):
             graycode[i] = 0
-    print("GRAYCODE :")
-    print(*graycode)
-    if last_grayCode != graycode:
-        last_grayCode = graycode
-        start_time = time.time()
-    else:
-        print("same graycode")
-
-    text_arr= []
-    for ind in range(len(GRAYCODE_TABLE)):
-        if (GRAYCODE_TABLE[ind] == str(graycode)):
-            print(ind)
-            print(PLANET_NAME[ind]) 
-            text_arr =  TEXTS[PLANET_NAME[ind]]
-
-    turnOnAllLights()
-
-    text = TEXTS['SCREENSAVER']
-    #Buttons
-    for i in range(len(PINS_IN)):
-        if GPIO.input(PINS_IN[i]):
-            print(BUTTON_NAMES[i])
-            print(text_arr[i])
-            text = text_arr[i]
-            changeLight(i)
-
-
-    drawText(screen, text, 400, 20, font, color_black, False)
-
-    
-    pygame.display.update()
-
-def main():
-    global screen
-    global color_black
-    global font
-    global img_net
-
-    time.sleep(1)
-        
-    pygame.init()
-    pygame.mouse.set_cursor((8,8),(0,0),(0,0,0,0,0,0,0,0),(0,0,0,0,0,0,0,0))
-
-    screen = pygame.display.set_mode((800, 480), pygame.NOFRAME)
-    #print('system font :', sysfont)
-   
-    GPIO.setmode(GPIO.BCM)
-    
-
-    for pin in PINS_IN:
-        GPIO.setup(pin, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
-    
-    for pin in GRAYCODE_PINS:
-        GPIO.setup(pin, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
-
-    for pin in PINS_OUT:
-        GPIO.setup(pin, GPIO.OUT)
-        GPIO.output(pin, 0)
-
-    color_black = pygame.Color("#000000")
-    img_net = pygame.image.load(dir + '/images/network_strength.png')   
-    
-    font = pygame.font.Font(pygame.font.match_font('FS ALdrin'), 16)
-    
-  
-#####################################################################
-if __name__ == '__main__':
-    try:
-        main()
-    except KeyboardInterrupt:
-        pass
-    finally:
-        RUNNING = True
-        print("finally")
-        #GPIO.cleanup()
-
-def timeout():
-    global start_time
-    start_time = time.time()
-    print("timeout")
-
-
-start_time = time.time()
-timeout_seconds = 60
-last_grayCode = ""
-
-# main game loop
-while RUNNING:
-    
-    update()
-
-    time.sleep(0.1)
-    now_time = time.time()
-    elapsed_time = now_time - start_time
-    #print(elapsed_time)
-    if (elapsed_time >= timeout_seconds):
-        timeout()
-
-
-    for event in pygame.event.get():
-        if (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
-            RUNNING = False
-            pygame.quit()
-            GPIO.cleanup()
             
-    pygame.time.Clock().tick(FPS)
-#End
+	str_graycode = makeGrayCodeString(graycode)
+	#drawText(screen, "GREYCODE " + str(graycode), 400, 400, font, pygame.Color("#ffffff"), False)
+	
+	#print(str_graycode)
+	planettext = TEXTS["SCREENSAVER"]
+	if str_graycode != last_grayCode:
+		last_grayCode = str_graycode
+	# maybe ask for last graycode
+	else:
+		#print(str_graycode)
+		planet = findChosenPlanet(str_graycode)
+		text_arr = TEXTS[planet]
+		turnOnAllLights()
+		drawText(screen, "PLANET "+ planet, 50, 100, font, pygame.Color("#000000"), False)
+		planettext = findCorrectText(planet, text_arr)
+    	#drawText(screen, planettext, 50, 150, font, pygame.Color("#ffffff"), False)
+    	textbox = pygame.Rect(50,150, 500, 100)
+    	drawTextBox(screen, planettext, pygame.Color("#000000"), textbox, font, True)
+    	
+	pygame.display.update()
+	
+if __name__ == '__main__':
+	try:
+		init()
+	except KeyboardInterrupt:
+		pass
+	finally: 
+		RUNNING = True
+		print("finally")
+
+
+while RUNNING:
+	update()
+	
+	for event in pygame.event.get():
+		if (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+			RUNNING = False
+			pygame.quit()
+			GPIO.cleanup()
+
